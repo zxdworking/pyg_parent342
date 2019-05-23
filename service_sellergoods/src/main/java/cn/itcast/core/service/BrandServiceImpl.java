901ffd3f820e4d7128be40b07f2091da3e89a585
123 <123@itcast.cn>
2019-05-23 18:26:51 +0800

    柳岩在23行新增一句代码

--- service_sellergoods/src/main/java/cn/itcast/core/service/BrandServiceImpl.java
+++ service_sellergoods/src/main/java/cn/itcast/core/service/BrandServiceImpl.java
@@ -20,6 +20,7 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public List<Brand> findAll() {
         System.out.println("柳岩新建一个分支");
+        System.out.println("柳岩++++++++++");
         return brandDao.selectByExample(null);
     }
 }
